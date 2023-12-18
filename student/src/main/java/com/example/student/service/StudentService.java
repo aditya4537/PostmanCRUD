9f556e717299684945ba6839e35a072d319468fa
aditya4537 <153008004+aditya4537@users.noreply.github.com>
2023-12-18 18:01:57 +0530

    Add files via upload

---
+++ student/src/main/java/com/example/student/service/StudentService.java
@@ -0,0 +1,15 @@
+package com.example.student.service;
+
+import java.util.List;
+
+import com.example.student.model.Student;
+
+public interface StudentService {
+	public List<Student> studentDetails();
+	
+	Student save(Student student);
+	
+	void delete(long id);
+	
+	public Student edit(Student student);
+}
